f9a7827f8dd29601b150370e91c0405fdb70f30a
Frederico Gassen <fredericogassen@gmail.com>
2022-08-29 20:38:11 -0300

    fix home fragment

--- features/home/src/main/java/com/android/itapiruba/features/home/HomeFragment.kt
+++ features/home/src/main/java/com/android/itapiruba/features/home/HomeFragment.kt
@@ -5,15 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.android.itapiruba.features.home.databinding.HomeFragmentHomeBinding
+import com.android.itapiruba.features.home.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: HomeFragmentHomeBinding? = null
+    private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        return HomeFragmentHomeBinding.inflate(inflater, container, false)
+        return FragmentHomeBinding.inflate(inflater, container, false)
             .also { _binding = it }
             .root
     }
